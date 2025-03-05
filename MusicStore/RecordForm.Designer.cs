f4379b8fad751066902bc257296f1fdf5d0d8e0d
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-05 21:18:28 +0300

    Implement RecordForm with data binding and validation
    Добавлена форма RecordForm с привязкой данных к объекту Record.
    Реализована валидация вводимых данных:
    - Проверка наличия названия записи
    - Проверка корректности цены продажи относительно себестоимости
    Привязаны все поля формы к соответствующим свойствам объекта Record.

---
+++ MusicStore/RecordForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace MusicStore
+{
+    partial class RecordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.nudTrackCount = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nudReleaseYear = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nudCostPrice = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nudSellingPrice = new System.Windows.Forms.NumericUpDown();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTrackCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReleaseYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCostPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSellingPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название запись";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Исполнитель";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Издатель";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(25, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Жанр";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTitle.Location = new System.Drawing.Point(187, 22);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(370, 27);
+            this.txtTitle.TabIndex = 4;
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtArtist.Location = new System.Drawing.Point(187, 58);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.Size = new System.Drawing.Size(370, 27);
+            this.txtArtist.TabIndex = 5;
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPublisher.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPublisher.Location = new System.Drawing.Point(187, 93);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(370, 27);
+            this.txtPublisher.TabIndex = 6;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtGenre.Location = new System.Drawing.Point(187, 126);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(370, 27);
+            this.txtGenre.TabIndex = 7;
+            // 
+            // nudTrackCount
+            // 
+            this.nudTrackCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nudTrackCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudTrackCount.Location = new System.Drawing.Point(203, 159);
+            this.nudTrackCount.Name = "nudTrackCount";
+            this.nudTrackCount.Size = new System.Drawing.Size(43, 22);
+            this.nudTrackCount.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(22, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Количество треков";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(22, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Год выпуска";
+            // 
+            // nudReleaseYear
+            // 
+            this.nudReleaseYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nudReleaseYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudReleaseYear.Location = new System.Drawing.Point(203, 194);
+            this.nudReleaseYear.Name = "nudReleaseYear";
+            this.nudReleaseYear.Size = new System.Drawing.Size(43, 22);
+            this.nudReleaseYear.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(22, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 22);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Себестоимость";
+            // 
+            // nudCostPrice
+            // 
+            this.nudCostPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nudCostPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudCostPrice.Location = new System.Drawing.Point(203, 227);
+            this.nudCostPrice.Name = "nudCostPrice";
+            this.nudCostPrice.Size = new System.Drawing.Size(43, 22);
+            this.nudCostPrice.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(22, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 22);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Цена продаж";
+            // 
+            // nudSellingPrice
+            // 
+            this.nudSellingPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nudSellingPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudSellingPrice.Location = new System.Drawing.Point(203, 260);
+            this.nudSellingPrice.Name = "nudSellingPrice";
+            this.nudSellingPrice.Size = new System.Drawing.Size(43, 22);
+            this.nudSellingPrice.TabIndex = 14;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOK.Location = new System.Drawing.Point(482, 255);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 29);
+            this.btnOK.TabIndex = 16;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            // 
+            // RecordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.nudSellingPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.nudCostPrice);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nudReleaseYear);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nudTrackCount);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtPublisher);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RecordForm";
+            this.Text = "RecordForm";
+            ((System.ComponentModel.ISupportInitialize)(this.nudTrackCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudReleaseYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCostPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSellingPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.NumericUpDown nudTrackCount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nudReleaseYear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nudCostPrice;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown nudSellingPrice;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ MusicStore/RecordForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicStore
+{
+    public partial class RecordForm : Form
+    {
+        public Record Record { get; private set; }
+
+        public RecordForm(Record record = null)
+        {
+            InitializeComponent();
+
+            Record = record ?? new Record();
+            BindControls();
+        }
+
+        private void BindControls()
+        {
+            txtTitle.DataBindings.Add("Text", Record, "Title");
+            txtArtist.DataBindings.Add("Text", Record, "Artist");
+            txtPublisher.DataBindings.Add("Text", Record, "Publisher");
+            nudTrackCount.DataBindings.Add("Value", Record, "TrackCount");
+            txtGenre.DataBindings.Add("Text", Record, "Genre");
+            nudReleaseYear.DataBindings.Add("Value", Record, "ReleaseYear");
+            nudCostPrice.DataBindings.Add("Value", Record, "CostPrice");
+            nudSellingPrice.DataBindings.Add("Value", Record, "SellingPrice");
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                Record.Title = Record.Title;
+                Record.Artist = Record.Artist;
+                Record.Publisher = Record.Publisher;
+                Record.TrackCount = Record.TrackCount;
+                Record.Genre = Record.Genre;
+                Record.ReleaseYear = Record.ReleaseYear;
+                Record.CostPrice = Record.CostPrice;
+                Record.SellingPrice = Record.SellingPrice;
+
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                MessageBox.Show("Введите название записи");
+                return false;
+            }
+
+            if (Record.SellingPrice <= Record.CostPrice)
+            {
+                MessageBox.Show("Цена продажи должна быть больше себестоимости");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
