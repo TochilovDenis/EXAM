c2daad63fd73a15e67171b14df3ff98c2fa840ba
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-10 19:41:22 +0300

    feat: реализация моделей музыкального магазина - Добавлен класс Sale для управления продажами - Добавлен класс StockOperation для отслеживания запасов - Добавлен класс Promotion для управления акциями - Добавлен класс ReservedRecord для бронирования записей - Все модели содержат базовые свойства и связи с записями

---
+++ MusicStore/Models.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore
+{
+    class Models
+    {
+        public class Sale
+        {
+            public int Id { get; set; }
+            public int RecordId { get; set; }
+            public DateTime SaleDate { get; set; }
+            public decimal Price { get; set; }
+            public string CustomerName { get; set; }
+        }
+
+        public class StockOperation
+        {
+            public int Id { get; set; }
+            public int RecordId { get; set; }
+            public DateTime OperationDate { get; set; }
+            public string OperationType { get; set; }
+            public int Quantity { get; set; }
+            public string Reason { get; set; }
+        }
+
+        public class Promotion
+        {
+            public int Id { get; set; }
+            public int RecordId { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public decimal DiscountPercentage { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class ReservedRecord
+        {
+            public int Id { get; set; }
+            public int RecordId { get; set; }
+            public DateTime ReserveDate { get; set; }
+            public DateTime ExpireDate { get; set; }
+            public string CustomerName { get; set; }
+            public bool IsConfirmed { get; set; }
+        }
+
+    }
+}
