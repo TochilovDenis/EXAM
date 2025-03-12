98eb0f2495f6b1d398a79c45a3b7ada106211801
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-12 19:15:33 +0300

    Добавить форму бронирования записей в музыкальном магазине
    Потому что:
    - Необходима функциональность бронирования записей
    - Требуется пользовательский интерфейс для ввода данных
    - Нужна возможность установки даты истечения брони
    
    Изменения включают:
    - Создание формы с полями для записи, исполнителя и покупателя
    - Добавление календаря для выбора даты истечения брони
    - Реализацию кнопок OK и Отмена
    - Обработку ресурсов формы через Dispose

---
+++ MusicStore/ReserveForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MusicStore
+{
+    partial class ReserveForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.txtRecord = new System.Windows.Forms.TextBox();
+            this.lblArtist = new System.Windows.Forms.Label();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.lblExpireDate = new System.Windows.Forms.Label();
+            this.dtpExpireDate = new System.Windows.Forms.DateTimePicker();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.Location = new System.Drawing.Point(16, 18);
+            this.lblRecord.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(57, 16);
+            this.lblRecord.TabIndex = 0;
+            this.lblRecord.Text = "Запись:";
+            // 
+            // txtRecord
+            // 
+            this.txtRecord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRecord.Location = new System.Drawing.Point(139, 15);
+            this.txtRecord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRecord.Name = "txtRecord";
+            this.txtRecord.ReadOnly = true;
+            this.txtRecord.Size = new System.Drawing.Size(265, 22);
+            this.txtRecord.TabIndex = 1;
+            // 
+            // lblArtist
+            // 
+            this.lblArtist.AutoSize = true;
+            this.lblArtist.Location = new System.Drawing.Point(16, 50);
+            this.lblArtist.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblArtist.Name = "lblArtist";
+            this.lblArtist.Size = new System.Drawing.Size(97, 16);
+            this.lblArtist.TabIndex = 2;
+            this.lblArtist.Text = "Исполнитель:";
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArtist.Location = new System.Drawing.Point(139, 47);
+            this.txtArtist.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.ReadOnly = true;
+            this.txtArtist.Size = new System.Drawing.Size(265, 22);
+            this.txtArtist.TabIndex = 3;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(16, 82);
+            this.lblCustomerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(89, 16);
+            this.lblCustomerName.TabIndex = 4;
+            this.lblCustomerName.Text = "Покупатель:";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCustomerName.Location = new System.Drawing.Point(139, 79);
+            this.txtCustomerName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(265, 22);
+            this.txtCustomerName.TabIndex = 5;
+            // 
+            // lblExpireDate
+            // 
+            this.lblExpireDate.AutoSize = true;
+            this.lblExpireDate.Location = new System.Drawing.Point(16, 114);
+            this.lblExpireDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblExpireDate.Name = "lblExpireDate";
+            this.lblExpireDate.Size = new System.Drawing.Size(114, 16);
+            this.lblExpireDate.TabIndex = 6;
+            this.lblExpireDate.Text = "Дата истечения:";
+            // 
+            // dtpExpireDate
+            // 
+            this.dtpExpireDate.Location = new System.Drawing.Point(139, 111);
+            this.dtpExpireDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpExpireDate.Name = "dtpExpireDate";
+            this.dtpExpireDate.Size = new System.Drawing.Size(265, 22);
+            this.dtpExpireDate.TabIndex = 7;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(139, 155);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "ОК";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(305, 155);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // ReserveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 207);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.dtpExpireDate);
+            this.Controls.Add(this.lblExpireDate);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.lblArtist);
+            this.Controls.Add(this.txtRecord);
+            this.Controls.Add(this.lblRecord);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ReserveForm";
+            this.Text = "Бронирование записи";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
