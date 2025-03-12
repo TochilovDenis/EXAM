3c6030eeed0958c5c84b900d199c5aa11d33dea1
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-12 20:31:08 +0300

    feat(form/main): Добавлена элементы управления: - FlowLayoutPanel с кнопками управления:   * Поиск записей

--- MusicStore/Form1.Designer.cs
+++ MusicStore/Form1.Designer.cs
@@ -28,40 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
             this.dgvRecords = new System.Windows.Forms.DataGridView();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnSale_Click = new System.Windows.Forms.Button();
-            this.btnStock = new System.Windows.Forms.Button();
-            this.btnPromotions = new System.Windows.Forms.Button();
             this.btnReserve = new System.Windows.Forms.Button();
+            this.btnPromotions = new System.Windows.Forms.Button();
+            this.btnStock = new System.Windows.Forms.Button();
+            this.btnSale_Click = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.search = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecords)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnAdd
+            // dgvRecords
             // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnAdd.Location = new System.Drawing.Point(3, 3);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(214, 30);
-            this.btnAdd.TabIndex = 0;
-            this.btnAdd.Text = "Добавить новый запись";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            this.dgvRecords.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dgvRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecords.Location = new System.Drawing.Point(23, 233);
+            this.dgvRecords.Name = "dgvRecords";
+            this.dgvRecords.RowHeadersWidth = 51;
+            this.dgvRecords.RowTemplate.Height = 24;
+            this.dgvRecords.Size = new System.Drawing.Size(1271, 377);
+            this.dgvRecords.TabIndex = 3;
             // 
-            // btnEdit
+            // btnReserve
             // 
-            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnEdit.Location = new System.Drawing.Point(223, 3);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(272, 30);
-            this.btnEdit.TabIndex = 1;
-            this.btnEdit.Text = "Редактировать записи";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click_1);
+            this.btnReserve.Location = new System.Drawing.Point(543, 39);
+            this.btnReserve.Name = "btnReserve";
+            this.btnReserve.Size = new System.Drawing.Size(174, 35);
+            this.btnReserve.TabIndex = 6;
+            this.btnReserve.Text = "Бронирование";
+            this.btnReserve.UseVisualStyleBackColor = true;
+            this.btnReserve.Click += new System.EventHandler(this.btnReserve_Click);
+            // 
+            // btnPromotions
+            // 
+            this.btnPromotions.Location = new System.Drawing.Point(363, 39);
+            this.btnPromotions.Name = "btnPromotions";
+            this.btnPromotions.Size = new System.Drawing.Size(174, 35);
+            this.btnPromotions.TabIndex = 5;
+            this.btnPromotions.Text = "Акции";
+            this.btnPromotions.UseVisualStyleBackColor = true;
+            this.btnPromotions.Click += new System.EventHandler(this.btnPromotions_Click);
+            // 
+            // btnStock
+            // 
+            this.btnStock.Location = new System.Drawing.Point(183, 39);
+            this.btnStock.Name = "btnStock";
+            this.btnStock.Size = new System.Drawing.Size(174, 35);
+            this.btnStock.TabIndex = 4;
+            this.btnStock.Text = "Операции склада";
+            this.btnStock.UseVisualStyleBackColor = true;
+            this.btnStock.Click += new System.EventHandler(this.btnStock_Click);
+            // 
+            // btnSale_Click
+            // 
+            this.btnSale_Click.Location = new System.Drawing.Point(3, 39);
+            this.btnSale_Click.Name = "btnSale_Click";
+            this.btnSale_Click.Size = new System.Drawing.Size(174, 35);
+            this.btnSale_Click.TabIndex = 3;
+            this.btnSale_Click.Text = "Продать";
+            this.btnSale_Click.UseVisualStyleBackColor = true;
+            this.btnSale_Click.Click += new System.EventHandler(this.btnSale_Click_Click);
             // 
             // btnDelete
             // 
@@ -74,16 +107,27 @@
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
             // 
-            // dgvRecords
+            // btnEdit
             // 
-            this.dgvRecords.BackgroundColor = System.Drawing.SystemColors.Info;
-            this.dgvRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRecords.Location = new System.Drawing.Point(23, 160);
-            this.dgvRecords.Name = "dgvRecords";
-            this.dgvRecords.RowHeadersWidth = 51;
-            this.dgvRecords.RowTemplate.Height = 24;
-            this.dgvRecords.Size = new System.Drawing.Size(1271, 450);
-            this.dgvRecords.TabIndex = 3;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEdit.Location = new System.Drawing.Point(223, 3);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(272, 30);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Редактировать записи";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click_1);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(214, 30);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Добавить новый запись";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
             // 
             // flowLayoutPanel1
             // 
@@ -94,51 +138,22 @@
             this.flowLayoutPanel1.Controls.Add(this.btnStock);
             this.flowLayoutPanel1.Controls.Add(this.btnPromotions);
             this.flowLayoutPanel1.Controls.Add(this.btnReserve);
+            this.flowLayoutPanel1.Controls.Add(this.search);
             this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(728, 120);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(728, 124);
             this.flowLayoutPanel1.TabIndex = 4;
             // 
-            // btnSale_Click
+            // search
             // 
-            this.btnSale_Click.Location = new System.Drawing.Point(3, 39);
-            this.btnSale_Click.Name = "btnSale_Click";
-            this.btnSale_Click.Size = new System.Drawing.Size(174, 35);
-            this.btnSale_Click.TabIndex = 3;
-            this.btnSale_Click.Text = "Продать";
-            this.btnSale_Click.UseVisualStyleBackColor = true;
-            this.btnSale_Click.Click += new System.EventHandler(this.btnSale_Click_Click);
-            // 
-            // btnStock
-            // 
-            this.btnStock.Location = new System.Drawing.Point(183, 39);
-            this.btnStock.Name = "btnStock";
-            this.btnStock.Size = new System.Drawing.Size(174, 35);
-            this.btnStock.TabIndex = 4;
-            this.btnStock.Text = "Операции склада";
-            this.btnStock.UseVisualStyleBackColor = true;
-            this.btnStock.Click += new System.EventHandler(this.btnStock_Click);
-            // 
-            // btnPromotions
-            // 
-            this.btnPromotions.Location = new System.Drawing.Point(363, 39);
-            this.btnPromotions.Name = "btnPromotions";
-            this.btnPromotions.Size = new System.Drawing.Size(174, 35);
-            this.btnPromotions.TabIndex = 5;
-            this.btnPromotions.Text = "Акции";
-            this.btnPromotions.UseVisualStyleBackColor = true;
-            this.btnPromotions.Click += new System.EventHandler(this.btnPromotions_Click);
-            // 
-            // btnReserve
-            // 
-            this.btnReserve.Location = new System.Drawing.Point(543, 39);
-            this.btnReserve.Name = "btnReserve";
-            this.btnReserve.Size = new System.Drawing.Size(174, 35);
-            this.btnReserve.TabIndex = 6;
-            this.btnReserve.Text = "Бронирование";
-            this.btnReserve.UseVisualStyleBackColor = true;
-            this.btnReserve.Click += new System.EventHandler(this.btnReserve_Click);
+            this.search.Location = new System.Drawing.Point(3, 80);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(174, 35);
+            this.search.TabIndex = 7;
+            this.search.Text = "Поиск";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
             // 
             // Form1
             // 
@@ -148,24 +163,26 @@
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.dgvRecords);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Музыкальный магазин";
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecords)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.DataGridView dgvRecords;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button btnSale_Click;
-        private System.Windows.Forms.Button btnStock;
-        private System.Windows.Forms.Button btnPromotions;
         private System.Windows.Forms.Button btnReserve;
+        private System.Windows.Forms.Button btnPromotions;
+        private System.Windows.Forms.Button btnStock;
+        private System.Windows.Forms.Button btnSale_Click;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button search;
     }
 }
 
