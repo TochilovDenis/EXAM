4c46fb0f6864a537a1d0ac087e8b0c99f6123ef2
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-12 19:00:46 +0300

    Добавить обработчик события отмены в RecordForm
    * Реализована функциональность закрытия формы при нажатии кнопки Cancel
    * Установка DialogResult как Cancel перед закрытием окна

--- MusicStore/RecordForm.cs
+++ MusicStore/RecordForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MusicStore
@@ -73,6 +66,11 @@ namespace MusicStore
             }
 
             return true;
+        }
+private void btnCancel_Click_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
     }
 }
