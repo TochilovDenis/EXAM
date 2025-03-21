04f606be6ac019a6a2fc0480569ab44880ec7c2e
TochilovDenis <denis-tochilov@yandex.ru>
2025-03-21 16:10:58 +0300

    feat(register-form): Добавлена форма регистрации пользователя
    * Реализована асинхронная регистрация пользователей через UserRepository
    * Добавлена валидация входных данных:
      + Проверка на пустые поля логина и пароля
      + Подтверждение пароля
      + Минимальная длина пароля (6 символов)
    * Реализовано отображение сообщений об ошибках и успехе
    * Добавлена возможность выбора роли пользователя (Пользователь/Администратор)
    * Реализована обработка исключений при регистрации

---
+++ MusicStore/RegisterForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MusicStore
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Требуемая переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">true если управляемый ресурс должен быть удален; иначе false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, сгенерированный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.cbRole = new System.Windows.Forms.ComboBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLogin.Location = new System.Drawing.Point(20, 45);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(200, 22);
+            this.txtLogin.TabIndex = 0;
+            this.txtLogin.Text = "Логин";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(20, 105);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Пароль";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtConfirmPassword.Location = new System.Drawing.Point(20, 165);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.PasswordChar = '*';
+            this.txtConfirmPassword.Size = new System.Drawing.Size(200, 22);
+            this.txtConfirmPassword.TabIndex = 2;
+            this.txtConfirmPassword.Text = "Повторный пароль";
+            // 
+            // cbRole
+            // 
+            this.cbRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRole.FormattingEnabled = true;
+            this.cbRole.Items.AddRange(new object[] {
+            "Пользователь",
+            "Администратор"});
+            this.cbRole.Location = new System.Drawing.Point(20, 225);
+            this.cbRole.Name = "cbRole";
+            this.cbRole.Size = new System.Drawing.Size(200, 24);
+            this.cbRole.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(20, 260);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(200, 27);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Зарегистрироваться";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.BtnRegister_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(20, 293);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(200, 27);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.cbRole);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RegisterForm";
+            this.Text = "Регистрация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.ComboBox cbRole;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ MusicStore/RegisterForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MusicStore
+{
+    public partial class RegisterForm : Form
+    {
+       private readonly UserRepository _userRepository;
+
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+            _userRepository = new UserRepository();
+        }
+
+        private async void BtnRegister_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                try
+                {
+                    var login = txtLogin.Text.Trim();
+                    var password = txtPassword.Text;
+                    var role = Convert.ToString(cbRole.SelectedItem);
+
+                    await _userRepository.RegisterUserAsync(login, password, role);
+                    MessageBox.Show("Регистрация успешна!");
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при регистрации: {ex.Message}");
+                }
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("Введите логин");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Введите пароль");
+                return false;
+            }
+
+            if (txtPassword.Text != txtConfirmPassword.Text)
+            {
+                MessageBox.Show("Пароли не совпадают");
+                return false;
+            }
+
+            if (txtPassword.Text.Length < 6)
+            {
+                MessageBox.Show("Пароль должен быть не менее 6 символов");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+    }
+}
